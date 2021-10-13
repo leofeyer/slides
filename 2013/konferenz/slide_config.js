7decc5b687a54f2944eb4bf9546f6a85a494c309
Leo Feyer <github@contao.org>
2021-10-13 13:16:45 +0200

    Initial commit

---
+++ 2013/konferenz/slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+
+  // Slide settings
+  settings: {
+    title: 'Keynote',
+    subtitle: 'Contao-Konferenz 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Leo Feyer',
+    company: 'Lead Developer',
+    gplus: 'https://plus.google.com/115656511940616195446',
+    twitter: '@leofeyer',
+    www: 'https://contao.org',
+    github: 'https://github.com/leofeyer'
+  }]
+
+};
